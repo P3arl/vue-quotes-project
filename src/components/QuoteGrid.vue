b11517d9b80139f0f179f6bc357008b6f0f9bc19
Parveen Parmar <pparmar@wsgc.com>
2020-03-29 00:05:53 +0530

    edited files till delete quote functionality

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+        <app-Quote v-for="(quote, index) in quotes" :key="quote"
+        @click.native="deleteQuote(index)"
+        >
+            {{ quote }}
+        </app-Quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+
+export default {
+    props: ['quotes'],
+    methods: {
+        deleteQuote(index) {
+            this.$emit('quoteDeleted', index)
+        }
+    },
+    components: {
+        appQuote: Quote
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
